9404913a2cb96b53bdf4caffb040fbfa7d9b50ae
acraine <41396237+acraine@users.noreply.github.com>
2024-07-19 15:04:22 -0700

    Delete scripts directory

--- scripts/getNegWorkFractions.m
+++
@@ -1,7 +0,0 @@
-function [NegWorkFraction] = getNegWorkFractions(work,areas)
-
-    workNegLocation   = find(work < 0);
-    workNegVolume     = sum(areas(workNegLocation));
-    NegWorkFraction = workNegVolume./sum(areas);
-
-end
